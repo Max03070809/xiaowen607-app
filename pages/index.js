30d32e8ff3b67c2932b210c3cd6d9e66e13038d1
Max <xuechenqi@yeah.net>
2025-06-05 10:49:39 +0800

    clean push

---
+++ pages/index.js
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="min-h-screen bg-green-50 text-gray-800 flex items-center justify-center">
+      <h1 className="text-4xl font-bold">孝闻街 607 餐酒咖啡</h1>
+    </div>
+  )
+}
